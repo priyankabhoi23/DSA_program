5f071c772b0dde03a2db9bf98e193e778e610e4e
priyankabhoi23 <148071576+priyankabhoi23@users.noreply.github.com>
2024-05-02 19:19:21 +0530

    Update 1search_element.py

--- 1search_element.py
+++ 1search_element.py
@@ -1,5 +1,5 @@
-arr=list(map(int,input("Enter array element:").split()))
 n,x=map(int,input("Enter elements to search").split())
+arr=list(map(int,input("Enter array element:").split()))
 if n in arr and x in arr:
     print("elements found")
 else:
