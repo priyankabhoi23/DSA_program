0d9728dcb5bfad9aa71822f8202646ec1eb94543
priyankabhoi23 <148071576+priyankabhoi23@users.noreply.github.com>
2024-05-19 19:18:50 +0530

    Add files via upload

---
+++ 8string_operation.py
@@ -0,0 +1,8 @@
+string = "Hello World"
+print(string[:5])        
+print(string[6:])        
+print(string[2:9])      
+print(string[::-1]) 
+print(string[0:5].upper()+string[6:].upper())     
+print(string.upper())
+print(string.lower())
